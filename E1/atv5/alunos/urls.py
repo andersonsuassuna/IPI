d7545bcea9c8696063d39cda866aec02ea731962
Anderson Suassuna <andersonssgf7@gmail.com>
2026-05-14 17:08:35 -0300

    atv5

--- E1/atv5/alunos/urls.py
+++ E1/atv5/alunos/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_alunos, lista_alunos, novo_alunos, detalhe_alunos
+from .views import lista_alunos, novo_alunos, detalhe_alunos
 
 app_name='alunos'
 
 urlpatterns = [
-    path('', home_alunos, name='home_alunos'),
     path('lista/', lista_alunos, name='lista'),
     path('novo/', novo_alunos, name='novo'),
     path('detalhe/', detalhe_alunos, name='detalhe')
--- E1/atv5/alunos/views.py
+++ E1/atv5/alunos/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_alunos(request):
-    return render(request, 'home_alunos.html')
 
 def lista_alunos(request):
-    return render(request, 'lista_alunos.html')
+    return render(request, '../templates/alunos/lista_alunos.html')
 
 def novo_alunos(request):
-    return render(request, 'novo_alunos.html')
+    return render(request, '../templates/alunos/novo_alunos.html')
 
 def detalhe_alunos(request):
-    return render(request, 'detalhe_alunos.html')
+    return render(request, '../templates/alunos/detalhe_alunos.html')
